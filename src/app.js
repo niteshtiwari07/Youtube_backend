909afb73476981b21b9ca285dcf64d328cb71e92
Nitesh <niteshtiwari0009@gmail.com>
2026-08-31 23:15:43 +0530

    upd

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(Express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
-export { app }
+export default app;
