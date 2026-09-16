8f0397fbedc828f871917f8c58a492c87caa30c5
Nitesh <niteshtiwari0009@gmail.com>
2026-09-16 11:31:14 +0530

    add getUserChannelProfile function to retrieve channel profile with subscriber counts

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -242,10 +242,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(new ApiResponse(
-        200,
-        req.user,
-        "User fetched successfully"
+    .json(new ApiResponse(200,req.user,"User fetched successfully"
     ))
 })
 
@@ -339,6 +336,48 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
     )
 })
 
+
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup :{
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup :{
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                ChannelSubscribedToCount: {
+                    $size:"$subsccribedTo"
+                }
+            }
+        }
+    ])
+})
 
 export {
     registerUser,
