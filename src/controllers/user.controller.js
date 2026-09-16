46581116ce9c3423422d83a2f0b61f042cd25622
Nitesh <niteshtiwari0009@gmail.com>
2026-09-16 16:29:55 +0530

    correct typo in subscribedTo field and enhance getUserChannelProfile response with subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -372,11 +372,37 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
                     $size: "$subscribers"
                 },
                 ChannelSubscribedToCount: {
-                    $size:"$subsccribedTo"
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond: {
+                        if: {$in: [req.user?._id, "subscribers.subscriber"]},
+                        then: true,
+                        else: false,
+                    }
                 }
+            }
+        },
+        {
+            $project: {
+                fulname: 1,
+                username: 1,
+                subscriberCount: 1,
+                ChannelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1  
             }
         }
     ])
+    if(!channel?.length){
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched sucessfully"))
 })
 
 export {
