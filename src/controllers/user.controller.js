70723e8b1dd68b8916b6ca85a44892a5157cbe82
Nitesh <niteshtiwari0009@gmail.com>
2026-09-17 09:52:06 +0530

    add comment, like, playlist, and tweet models with appropriate schemas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -53,7 +53,6 @@ const registerUser = asyncHandler( async (req, res) => {
     }
     //console.log(req.files);
 
-    const avatarLocalPath = req.files?.avatar?.[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
